d53b06d32e9fcf6149da516b279f0d25472292f9
jmkahn@hmc.edu <jkahn1618@DESKTOP-69RA3T1.localdomain>
2020-04-13 13:35:04 -0400

    Chaos game works for r=2 but doesn't work for a generalized r

--- ifs.py
+++ ifs.py
@@ -6,13 +6,15 @@ import random
 
 class ChaosGame(object):
 
-    def __init__(self, root):
+    def __init__(self, root, ratio):
         #make canvas
         self.canvas = tkinter.Canvas(root, width = 600, height = 600)
         self.canvas.pack()
 
         #make list of vertices
         self.vertices = [(5, 5), (5, 300), (300, 5)]
+
+        self.ratio = int(ratio)
 
         self.currentPoint = ()
         self.initializePoint()
@@ -44,44 +46,46 @@ class ChaosGame(object):
         point1 = random.choice(self.vertices)
         self.contraction(seed, point1)
 
-    def contraction(self, point1, point2, r=0.5):
-        x = (point1[0] + point2[0])*r
-        y = (point1[1] + point2[1])*r
+    #TODO: figure out the correct contraction equations for generalized r
+    def contraction(self, point1, point2):
+        r = self.ratio
+        x = (point1[0] + point2[0])/r
+        y = (point1[1] + point2[1])/r
+
         self.currentPoint = (x, y)
         self.drawPoint(x, y, 3)
 
 
-moreDots = 1
 
-def offswitch():
-    moreDots = 0
+moreDots = 1
 
 if __name__ == '__main__':
 
+    ratio = input("What contraction ratio do you want? Input an integer value. \n")
+
+
+
     root = tkinter.Tk()
 
-    w = ChaosGame(root)
+    w = ChaosGame(root, ratio)
     w.drawStartingPoints()
 
-    while (moreDots ==1):
+
+
+    def switch(event):
+        # print("I am happen now!!!! ")
+        global moreDots
+        moreDots = 0 #TODO: make it so that we can restart when click again
+
+    root.bind("<Button-1>", switch)
+
+    while (moreDots == 1):
     # print("this is current point", w.currentPoint)
     # print("a vertex:", random.choice(w.vertices))
         w.contraction(w.currentPoint, random.choice(w.vertices))
 
         root.update()
         time.sleep(0.01)
-
-
-
-
-    # root.bind(<Button-1>, offswitch)
-
-    # w.drawPoint(10, 10)
-    #
-    #
-
-    # w.drawPoint(15, 15)
-
 
 
     root.mainloop()
